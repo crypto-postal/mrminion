a64216f0d0bbbea194116ab037ca1e27fe8dbb1c
MrMinion <105944757+crypto-postal@users.noreply.github.com>
2022-08-08 10:49:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const btn = document.querySelector('[data-bs-target="#data"]');
 btn.innerHTML = 'Upload'+ ' <i class="fa-solid fa-arrow-down-short-wide"></i>';
-const icon_bar = document.querySelector('#navi');
-icon_bar.classList.add("icon-bar");
-icon_bar.append(btn);
-const light = document.querySelector('.light');
-light.setAttribute("class","grow");
+const navi = document.querySelector('#navi');
+navi.classList.add("icon-bar");
+const nav_a = document.querySelector('#navi a');
+nav_a.setAttribute('id','nav_a');
+const a_id = document.querySelector('#nav_a');
+a_id.append(btn);
+navi.append(a_id);
+
+const light = document.querySelector('div .light');
+light.setAttribute('class','grow');
 const cls = document.querySelectorAll('.icon span');
 cls.forEach(item=> item += item.classList.add("like"));
-const home = document.getElementById('home');
+let home = document.getElementById('home');
 home.setAttribute('class','top');
-document.querySelector('#home h2').innerHTML = '<i class="fas fa-hand-point-up" style="font-size:48px;color:#176023;"></i>';
+document.querySelector('#home h2').innerHTML = '<i class="fa-solid fa-hand-point-up" style="font-size:48px;color:#176023;"></i>';
